f4682de7faa06adced6088918cb87524d40562e0
Viktor1905 <webdevvva@gmail.com>
2025-01-18 17:49:21 +0300

    fix: replace sync function in 03 task by async function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const { stdin, stdout } = process;
+const fsPromisese = require('fs/promises');
 
 fs.readdir(
     path.join(__dirname, 'secret-folder'),
@@ -11,9 +12,19 @@ fs.readdir(
             if (file.isFile()) {
                 const basename = path.basename(`${file.name}`, path.extname(`${file.name}`));
                 const extname = path.extname(`${file.name}`).split('.').join('');
-                const fileMem = fs.statSync(path.join(__dirname, 'secret-folder', `${file.name}`)).size;
+                const fileMem = getFileSize(basename)
+                async function getFileSize(filePath) {
+                    try {
+                        const stats = await fsPromisese.stat(path.join(__dirname, 'secret-folder', `${file.name}`));
+                        return stats.size;
+                    } catch (err) {
+                        console.log(err.message);
+                    }
+                }
+                fileMem.then((size) => {
+                    stdout.write(`${basename} - ${extname} - ${size}\n` );
+                })
 
-                stdout.write(`${basename} - ${extname} - ${fileMem}\n` );
             }
         });
     },
